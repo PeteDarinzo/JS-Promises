dd66fc8deb48f3b1eac69b115d8bda836f0ce42e
Peter Darinzo <birel44@yahoo.com>
2021-11-17 13:31:29 -0700

    formatting

--- numbers/numbers.js
+++ numbers/numbers.js
@@ -2,9 +2,9 @@
 const numbersUrl = "http://numbersapi.com";
 
 
-/**
- * Make a request to the Numbers API (http://numbersapi.com/) to get a fact about your favorite number. (Make sure you get back JSON by including the json query key, specific to this API.
- */
+/********************************************************
+  * Make a request to the Numbers API (http://numbersapi.com/) to get a fact about your favorite number. (Make sure you get back JSON by including the json query key, specific to this API.
+********************************************************/
 
 // axios.get(`${numbersUrl}/44?json`)
 //     .then(res => {
@@ -12,9 +12,12 @@ const numbersUrl = "http://numbersapi.com";
 //         $("#fact-list").append(`<li>${fact}</li>`);
 //     })
 
-/**
+
+
+/********************************************************
  * Figure out how to get data on multiple numbers in a single request. Make that request and when you get the data back, put all of the number facts on the page.
- */
+ ********************************************************/
+
 
 // axios.get(`${numbersUrl}/1..5,10,11,12?json`)
 //     .then(res => {
@@ -25,9 +28,9 @@ const numbersUrl = "http://numbersapi.com";
 //     })
 
 
-/**
+/********************************************************
  * Use the API to get 4 facts on your favorite number. Once you have them all, put them on the page. It’s okay if some of the facts are repeats.
- */
+ ********************************************************/
 
 let numberFactPromises = [];
 
